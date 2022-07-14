089b7635d3ca9559a01069e3996b972156348fac
SG1515 <sunggu507@naver.com>
2022-07-14 17:00:27 +0900

    7강 등록에서 break 안된 버그 수정2

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -22,6 +22,7 @@ public class App {
                     String content = sc.nextLine();
                     System.out.println("작가 : ");
                     String author = sc.nextLine();
+                    break;
                 case "종료":
                     break outer; // 종료
             }
