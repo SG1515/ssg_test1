24df765552b7584116b6100b47487c21a12540dc
SG1515 <sunggu507@naver.com>
2022-07-15 15:44:40 +0900

    9강 목록 기능 구현

---
+++ src/main/java/com/ll/exam/WiseSaying.java
@@ -0,0 +1,12 @@
+package com.ll.exam;
+
+public class WiseSaying {
+    public int id;
+    public String author;
+    public String content;
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.author=author;
+        this.content =content;
+    }
+}
